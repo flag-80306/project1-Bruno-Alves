0cd2bfadf204e2247371a2d673f44077ecf0246a
Bruno Alves <93.bruno@gmail.com>
2023-12-06 20:01:40 +0000

    Added hero component

---
+++ src/components/Hero.jsx
@@ -0,0 +1,20 @@
+function Hero() {
+  return (
+    <>
+      <div className="menu">
+        <ul>
+          <li>Travel Blog</li>
+          <li>Home</li>
+          <li>Add Location</li>
+          <li>The Traveler</li>
+        </ul>
+      </div>
+      <div>
+        <h2>Location</h2>
+        <p>Date</p>
+      </div>
+    </>
+  );
+}
+
+export default Hero;
