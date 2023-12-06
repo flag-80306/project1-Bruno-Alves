7a4d8904d508595415475a61c8978189aa8b785d
Bruno Alves <93.bruno@gmail.com>
2023-12-06 21:38:02 +0000

    hero changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,28 @@
 function Hero() {
   return (
     <>
-      <div className="menu">
-        <ul>
-          <li>Travel Blog</li>
-          <li>Home</li>
-          <li>Add Location</li>
-          <li>The Traveler</li>
-        </ul>
-      </div>
-      <div>
-        <h2>Location</h2>
-        <p>Date</p>
+      <div className="hero">
+        <div className="heroMenu">
+          <ul>
+            <li>
+              <a href="#">Travel Blog</a>
+            </li>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">Add Location</a>
+            </li>
+            <li>
+              <a href="#">The Traveler</a>
+            </li>
+          </ul>
+        </div>
+        <div className="heroDetail">
+          <img src="" alt="Image" />
+          <h2>Location</h2>
+          <p>Date</p>
+        </div>
       </div>
     </>
   );
