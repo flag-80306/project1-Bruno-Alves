b4db82a5532397571fd77686721e8b7f5caf04f3
Bruno Alves <93.bruno@gmail.com>
2023-12-10 18:56:50 +0000

    Added background image and positioned hero elements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,26 +2,29 @@ function Hero() {
   return (
     <>
       <div className="hero">
-        <div className="heroMenu">
-          <ul>
-            <li>
-              <a href="#">Travel Blog</a>
-            </li>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">Add Location</a>
-            </li>
-            <li>
-              <a href="#">The Traveler</a>
-            </li>
-          </ul>
-        </div>
-        <div className="heroDetail">
-          <img src="" alt="Image" />
-          <h2>Location</h2>
-          <p>Date</p>
+        <div className="gradientFilter">
+          <div className="heroNav">
+            <ul>
+              <li>
+                <a href="#">Travel Blog</a>
+              </li>
+              <li>
+                <a href="#">Home</a>
+              </li>
+              <li>
+                <a href="#">Add Location</a>
+              </li>
+              <li>
+                <a href="#">The Traveler</a>
+              </li>
+            </ul>
+          </div>
+
+          <div className="heroDetail">
+            <p>Latest Posts</p>
+            <h2>Berlin</h2>
+            <p>10/12/2023</p>
+          </div>
         </div>
       </div>
     </>
