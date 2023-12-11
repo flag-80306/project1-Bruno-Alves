02e0364da1518b329e6618cb89dfb8d860bee1de
Bruno Alves <93.bruno@gmail.com>
2023-12-11 22:11:34 +0000

    image folder moved to public

--- src/assets/data/cardsInfo.js
+++ src/assets/data/cardsInfo.js
@@ -1,31 +1,31 @@
 const cardsInfo = [
   {
-    imgURL: "../assets/images/buenos-aires-image-1.jpg",
+    imgURL: "images/buenos_aires_image_1.jpg",
     city: "Buenos Aires",
     date: "15/08/2008",
   },
   {
-    imgURL: "../assets/images/madrid.jpg",
+    imgURL: "../images/madrid.jpg",
     city: "Madrid",
     date: "05/02/2017",
   },
   {
-    imgURL: "../assets/images/madrid.jpg",
+    imgURL: "../images/madrid.jpg",
     city: "Lisboa",
     date: "10/06/2022",
   },
   {
-    imgURL: "../assets/images/madrid.jpg",
+    imgURL: "../images/madrid.jpg",
     city: "Lisboa",
     date: "10/06/2022",
   },
   {
-    imgURL: "../assets/images/madrid.jpg",
+    imgURL: "../images/madrid.jpg",
     city: "Lisboa",
     date: "10/06/2022",
   },
   {
-    imgURL: "../assets/images/madrid.jpg",
+    imgURL: "../images/madrid.jpg",
     city: "Lisboa",
     date: "10/06/2022",
   },
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,10 +8,6 @@ function Card() {
       {cardsInfo.map((trip, index) => (
         <div className="card" key={index}>
           <img src={trip.imgURL} alt={`Image for ${trip.city}`} />
-          <img
-            src="https://images.unsplash.com/photo-1585164917473-bad9e1c92755?q=80&w=2126&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-            alt={`Image for ${trip.city}`}
-          />
           <p>{trip.city}</p>
           <p>{trip.date}</p>
         </div>
