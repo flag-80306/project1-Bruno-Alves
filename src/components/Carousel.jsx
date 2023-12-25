66cc6bd8a896f8eeb1b48be5b1efb808f049c7b1
Bruno Alves <93.bruno@gmail.com>
2023-12-25 19:20:18 +0000

    carousel functionality done

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,26 +1,49 @@
+import { useState } from "react";
 import { BsArrowLeftCircleFill, BsArrowRightCircleFill } from "react-icons/bs";
 
 function Carousel({ city }) {
   console.log("Console.log city!!!");
   console.log(city);
+  const [slide, setSlide] = useState(0);
+
+  function nextSlide() {
+    setSlide(slide === city.slides.length - 1 ? 0 : slide + 1);
+  }
+
+  function prevSlide() {
+    setSlide(slide === 0 ? city.slides.length - 1 : slide - 1);
+  }
+
   return (
     <>
       <h3>Details from {city.name}</h3>
 
       <div className="carousel">
-        <BsArrowLeftCircleFill className="arrow arrow-left" />
-        {city.slides?.map((slide, index) => (
+        <BsArrowLeftCircleFill
+          className="arrow arrow-left"
+          onClick={prevSlide}
+        />
+        {city.slides?.map((slideSRC, index) => (
           <img
-            src={slide}
-            className="slide"
+            src={slideSRC}
+            className={slide === index ? "slide" : "slide slide-hidden"}
             alt={`Image of ${city.name}`}
             key={index}
           />
         ))}
-        <BsArrowRightCircleFill className="arrow arrow-right" />
+        <BsArrowRightCircleFill
+          className="arrow arrow-right"
+          onClick={nextSlide}
+        />
         <span className="indicators">
           {city.slides?.map((_, index) => (
-            <button key={index} onClick={null} className="indicator"></button>
+            <button
+              key={index}
+              onClick={() => setSlide(index)}
+              className={
+                slide === index ? "indicator" : "indicator indicator-inactive"
+              }
+            ></button>
           ))}
         </span>
       </div>
