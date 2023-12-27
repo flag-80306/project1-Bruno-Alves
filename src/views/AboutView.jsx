e0f85d39a5d100d595f47a4a8abd37a00ea9783c
Bruno Alves <93.bruno@gmail.com>
2023-12-27 17:24:49 +0000

    added some content to the AboutView

--- src/views/AboutView.jsx
+++ src/views/AboutView.jsx
@@ -3,31 +3,41 @@ function AboutView() {
     <>
       <div className="headerBackground"></div>
       <div className="spacer"></div>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
-      <h2>This is the About View</h2>
+      <div className="travelerContainer">
+        <div className="travelerPhoto">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/480px-User_icon_2.svg.png"
+            alt="Traveler Photo"
+          />
+          <span>Bruno Alves Aragao</span>
+          <span>30 anos</span>
+          <span>Carioca</span>
+        </div>
+        <div className="travelerDescription">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+          incidunt quidem tempora aperiam facilis omnis sequi explicabo rem
+          doloribus deleniti optio assumenda pariatur a veniam magni et,
+          consectetur excepturi sunt. Lorem ipsum dolor sit amet, consectetur
+          adipisicing elit. Veniam minus optio tempora, voluptas ipsum
+          necessitatibus ut repellendus corrupti ullam magnam reprehenderit
+          dignissimos nam! Eaque laborum corrupti aut aperiam placeat dolor.
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquam
+          necessitatibus itaque soluta, ipsa nostrum esse facere a, ducimus
+          incidunt omnis dolor placeat tempore nulla quis laboriosam earum?
+          Veniam, id porro. Lorem, ipsum dolor sit amet consectetur adipisicing
+          elit. Ipsa tenetur debitis ut et quod asperiores perferendis ipsum
+          saepe porro, numquam ad alias repellat ea accusamus nobis eos vero,
+          odit nihil.
+        </div>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+        <h2>This is the About View</h2>
+      </div>
       <h2>This is the About View</h2>
       <h2>This is the About View</h2>
       <h2>This is the About View</h2>
