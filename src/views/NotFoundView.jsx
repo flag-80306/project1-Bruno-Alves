c07d8692d706a80bbae00ee54693342e3e2857ad
Bruno Alves <93.bruno@gmail.com>
2024-01-17 16:02:17 +0000

    added page not found styles

--- src/views/NotFoundView.jsx
+++ src/views/NotFoundView.jsx
@@ -3,8 +3,8 @@ function NotFoundView() {
     <>
       <div className="headerBackground"></div>
       <div className="spacer"></div>
-      <h1>!!!404!!!</h1>
-      <h2>PAGE NOT FOUND</h2>
+      <h1 id="error">!!!404!!!</h1>
+      <h2 id="errorMessage">PAGE NOT FOUND</h2>
     </>
   );
 }
